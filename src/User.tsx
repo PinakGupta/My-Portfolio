5bfee6f6a7bb1465b1d22f9d45d5e1f976644605
PinakGupta <pinakgupta99@gmail.com>
2025-01-11 21:24:24 +0530

    name change

--- src/User.tsx
+++ src/User.tsx
@@ -1,6 +1,6 @@
 import { IconBrandGithub, IconBrandInstagram, IconBrandLeetcode, IconBrandLinkedin, IconBrandYoutube } from "@tabler/icons-react";
 const Info = {
-    name: "Chandrabhan Maurya",
+    name: "Pinak Gupta",
     stack: ["Software Engineer", "Full Stack Developer", "Competitive Programmer", "Freelancer", "Open Source Contributor"],
     bio: "I'm a passionate software engineer specialized in crafting efficient and scalable solutions, deliver top-notch web applications, and provide freelance services that exceed client expectations. Let's connect and create something amazing together!"
 }
